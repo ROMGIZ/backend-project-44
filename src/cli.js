57c8df24467a8517600ca55c2475fdf2395d5277
Roman Gizatulin <roman.gizatulin95@gmail.com>
2023-03-07 22:41:36 +0700

    step3

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
-import readlineSync from 'readline-sync';
 
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!');
 export const name = readlineSync.question('May I have your name?: ');
+console.log('Hello, ' + name);
