f4082216fde4b82bb96cc48c297ec9cb713b5b11
Roman Gizatulin <roman.gizatulin95@gmail.com>
2023-08-11 15:28:34 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import readlineSync from 'readline-sync';
 
-// Тут мы принимаем два параметра. Первый это описание задачи и вопрос getDescription. 
-// Второе это сам функционал игры getRoundData.
 const engine = (description, game) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
@@ -9,25 +7,17 @@ const engine = (description, game) => {
   console.log(description);
 
   for (let n = 0; n < 3; n += 1) {
-    //dataForGame принимает getRoundData
     const dataForGame = game();
     const [question, answer] = dataForGame;
-    // const question = dataForGame[0];
-    // const answer = dataForGame[1];
     console.log(`Question: ${question} `);
     const userAnswer = readlineSync.question('Your answer: ');
     if (answer !== userAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".`);
       console.log(`Let's try again, ${userName}!`);
       return;
-    } else {
-      console.log('Correct!');
-    }
-
-    if (n === 2) {
-      console.log(`Congratulations, ${userName}!`);
     }
   }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default engine;
