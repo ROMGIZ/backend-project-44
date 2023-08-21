e28dc97d34427526f1e9d0b702f1924dc4d3b173
Roman Gizatulin <roman.gizatulin95@gmail.com>
2023-08-21 17:04:57 +0700

    Добавил brain-games в bin

--- src/games/getDataForBrainProgression.js
+++ src/games/getDataForBrainProgression.js
@@ -1,22 +1,5 @@
 import getRandomIntNum from '../utils.js';
 
-// const getProgressionAndHiddenEl = () => {
-//   const firstNum = getRandomIntNum();
-//   const progressionDifference = getRandomIntNum();
-//   const indexHiddenEl = getRandomIntNum(0, 9);
-//   const arrProgression = [firstNum];
-//   let n = 1;
-//   do {
-//     arrProgression[n] = arrProgression[n - 1] + progressionDifference;
-//     n += 1;
-//   } while (n < 10);
-//   const hiddenEl = arrProgression[indexHiddenEl].toString();
-//   arrProgression[indexHiddenEl] = '..';
-//   const strProgression = arrProgression.join(' ');
-//   return [strProgression, hiddenEl];
-// };
-
-
 const getProgressionAndHiddenEl = () => {
   const firstNum = getRandomIntNum(1, 100);
   const progressionDifference = getRandomIntNum(1, 10);
@@ -37,7 +20,6 @@ const getProgressionAndHiddenEl = () => {
 
   return [strProgression, hiddenEl];
 };
-
 
 const getDescription = () => 'What number is missing in the progression?';
 
